086b375e8e9e39710f6573521ceec634cb361204
Matheus Rafael <matheusrafael77@hotmail.com>
2025-01-23 21:54:21 -0300

    first commit

--- src/br/com/alura/AppBanco.java
+++ src/br/com/alura/AppBanco.java
@@ -1,4 +1,21 @@
 package br.com.alura;
 
+import java.math.BigDecimal;
+
 public class AppBanco {
+    public static void main(String[] args) {
+        var cliente = new Cliente("João");
+        var conta = new Conta(cliente, new BigDecimal(150));
+
+        var operacaoSaque = new OperacaoSaque(conta, new BigDecimal(150));
+
+        Thread saqueJoao = new Thread(operacaoSaque);
+        Thread saqueMaria = new Thread(operacaoSaque);
+
+
+        saqueJoao.start();
+        saqueMaria.start();
+
+
+    }
 }
